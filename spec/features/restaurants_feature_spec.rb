90983eb154a86198f7c4549d526b8511eee5fd1a
Fadie Hannona <fadiehannona@gmail.com>
2014-11-03 14:26:49 +0000

    NOT FINISHED, lets a user view a restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -32,4 +32,19 @@ describe 'creating restaurants' do
 		expect(current_path).to eq '/restaurants'
 	end
 end
+
+	context 'viewing restaurants' do
+
+		before do
+			@kfc = Restaurant.create(name:'KFC')
+		end
+
+		it 'lets a user view a restaurant' do
+			visit '/restaurants'
+			click_link 'KFC'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq "/restaurants/#{@kfc.id}"
+		end
+	end
+
 end

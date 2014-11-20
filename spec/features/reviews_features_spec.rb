609e61a58215d47a22088f93cdd29b96f74313b8
Fadie Hannona <fadiehannona@gmail.com>
2014-11-20 18:28:27 +0000

    Returns 5,3,3.5 stars for reviews

--- spec/features/reviews_features_spec.rb
+++ spec/features/reviews_features_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 require 'helpers/user_sign_up'
+require 'helpers/reviews_helper_spec'
 
 describe 'reviewing' do
   before do
@@ -17,16 +18,6 @@ end
 
   it 'displays an average rating for all reviews' do
     user_sign_up
-    leave_review('So so', "3")
-        save_and_open_page
-    click_link('Sign out')
-    user_sign_up_two
-    leave_review('Great', "5")
-    expect(page).to have_content("Average rating: 4")
-  end
-
-  xit 'displays an average rating for all reviews' do
-    user_sign_up
     leave_review("so so", "3")
     click_link('Sign out')
     user_sign_up_two
@@ -40,8 +31,27 @@ end
     fill_in "Thoughts", with: "so so"
     select '3', from: 'Rating'
     click_button 'Leave Review'
-
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
+  it 'returns five black stars for five' do
+    restaurant = Restaurant.create(name: "The Ivy")
+    restaurant.reviews.create(rating: 5)
+    expect(restaurant.average_rating).to eq 5
+  end
+
+  it 'returns three black stars and two white stars for three' do
+    restaurant = Restaurant.create(name: "The Ivy")
+    restaurant.reviews.create(rating: 3)
+    expect(restaurant.average_rating).to eq 3
+  end
+
+  it 'returns four black stars and one white star for 3.5' do
+    restaurant = Restaurant.create(name: "The Ivy")
+    restaurant.reviews.create(rating: 3)
+    restaurant.reviews.create(rating: 4)
+    expect(restaurant.average_rating).to eq 3.5
+  end
+
 end
